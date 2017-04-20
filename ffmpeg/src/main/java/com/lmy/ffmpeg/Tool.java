1bc9580e22cdcd483ee463d55c21eaefa7480591
lmy <lmyooyo@gmail.com>
2017-04-20 14:56:26 +0800

    完善逻辑

--- ffmpeg/src/main/java/com/lmy/ffmpeg/Tool.java
+++ ffmpeg/src/main/java/com/lmy/ffmpeg/Tool.java
@@ -5,5 +5,16 @@ package com.lmy.ffmpeg;
  */
 
 public class Tool {
+    static {
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avdevice-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("ffmpeg");
+    }
+
     public native String version();
 }
