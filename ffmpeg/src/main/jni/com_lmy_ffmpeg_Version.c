c929025e41f70d989af131e9680f052063964b83
lmy <lmyooyo@gmail.com>
2017-04-20 18:47:16 +0800

    完善逻辑

--- ffmpeg/src/main/jni/com_lmy_ffmpeg_Version.c
+++ ffmpeg/src/main/jni/com_lmy_ffmpeg_Version.c
@@ -10,9 +10,11 @@
 #ifdef ANDROID
 #include <jni.h>
 #include <android/log.h>
-#define LOGE(format, ...)  __android_log_print(ANDROID_LOG_ERROR, "(>_<)", format, ##__VA_ARGS__)
+#define LOGE(format, ...)  __android_log_print(ANDROID_LOG_ERROR, "JNI", format, ##__VA_ARGS__)
+#define LOGI(format, ...)  __android_log_print(ANDROID_LOG_INFO,  "JNI", format, ##__VA_ARGS__)
 #else
-#define LOGE(format, ...)  printf("(>_<) " format "\n", ##__VA_ARGS__)
+#define LOGE(format, ...)  printf("JNI" format "\n", ##__VA_ARGS__)
+#define LOGI(format, ...)  printf("JNI" format "\n", ##__VA_ARGS__)
 #endif
 /* ffmpeg版本信息
  * Class:     com_lmy_ffmpeg_Version
