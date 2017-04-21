30f31661ca8e5b5877f7a6d09c894d994d13267b
lmy <lmyooyo@gmail.com>
2017-04-21 21:36:54 +0800

    完善逻辑

--- ffmpeg/src/main/java/com/lmy/ffmpeg/codec/MediaDecoder.java
+++ ffmpeg/src/main/java/com/lmy/ffmpeg/codec/MediaDecoder.java
@@ -17,10 +17,45 @@ public class MediaDecoder {
     }
 
     private AVFrame mFrame;
+    private int width, height;
+
+    public MediaDecoder() {
+        this.mFrame = new AVFrame();
+    }
+
+    public void setDataSource(String path) {
+        setDataSource(path, mFrame);
+    }
+
+    private native void setDataSource(String path, AVFrame frame);
+
+    public native int nextFrame();
+
+    private native void decode(String path, AVFrame frame);
+
+    public native void release();
 
     public void decode(String path) {
         decode(path, mFrame);
     }
 
-    private native void decode(String path, AVFrame frame);
+    public AVFrame getFrame() {
+        return mFrame;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
